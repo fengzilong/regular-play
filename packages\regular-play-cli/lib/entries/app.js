8c210f4904f14c615f92972d4ae3edd273924eed
mo <fengzilong1992@gmail.com>
2017-03-10 02:03:32 +0800

    Revert "update"
    This reverts commit e0f86d07bb80f877c3b777201b16835a327ce19f.

--- packages\regular-play-cli/lib/entries/app.js
+++
@@ -1,9 +0,0 @@
-import app from 'regular-play/dist/app';
-
-import 'regular-play/dist/app.css';
-
-app();
-
-if ( !location.hash ) {
-	location.href = '#!/';
-}
--- packages\regular-play-cli/lib/entries/preview.js
+++
@@ -1,6 +0,0 @@
-import preview from 'regular-play/dist/preview';
-import p from 'play-entry';
-
-import 'regular-play/dist/preview.css';
-
-preview( p );
